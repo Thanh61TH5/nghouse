e9d330a976ce3f39ab7cdc117a695182296c697c
Thanh61TH5 <giangthanh25092001@gmail.com>
2023-05-31 22:01:45 +0700

    create header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home"
+import Offers from "./pages/Offers"
+import Profile from "./pages/Profile"
+import SingUp from "./pages/SignUp"
+import SingIn from "./pages/SignIn"
+import ForgotPassword from "./pages/ForgotPassword"
+import Header from "./components/Header";
 function App() {
   return (
-      <h1 className="text-3xl font-bold bg-red-200">
-        Hello world!
-      </h1>
+     <>
+         <Router>
+             <Header/>
+             <Routes>
+                 <Route path ="/" element={<Home/>}></Route>
+                 <Route path ="/profile" element={<Profile/>}></Route>
+                 <Route path ="/sign-in" element={<SingIn/>}></Route>
+                 <Route path ="/sign-up" element={<SingUp/>}></Route>
+                 <Route path ="/forgot-password" element={<ForgotPassword/>}></Route>
+                 <Route path ="/offers" element={<Offers/>}></Route>
+             </Routes>
+         </Router>
+     </>
   );
 }
 
